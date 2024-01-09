ce4b354a2121691fe11981879b551ca8bd6833c9
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 02:43:20 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -5,4 +5,10 @@ data = pd.read_csv("Churn.csv")
 
 st.title("Telecommunication Churn")
 
-st.sidebar.radio("Navigation",["Visualisation","Predictor"])
+nav = st.sidebar.radio("Navigation",["Visualisation","Predictor"])
+
+if nav == ["Visualisation"]:
+  st.write("Youre on visualisation page")
+
+if nav == ["Predictor"]:
+  st.write("Predict")
