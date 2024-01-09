db5e2a11ba48ab6ef3967939e566ffa36885c870
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 00:54:54 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -22,5 +22,5 @@ st.date_input("Enter today's date")
 st.time_input("Enter the time")
 
 #Add a checkbox
-if st.checkbox("You accept the T&C", value=True):
+if st.checkbox("You accept the T&C"):
   st.write("Thankyou")
