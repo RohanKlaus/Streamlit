2e7386602d823cb3246df87663a95dd4155b27d8
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 02:15:32 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.title("Registration Form")
 
-first, last = st.beta_columns(2)
+first,last = st.beta_columns(2)
+
 first.text_input("First Name")
 last.text_input("Last Name")
