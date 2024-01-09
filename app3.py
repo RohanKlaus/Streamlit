2d46a4e806964c24c5121c26ae660686f6f9885c
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 02:21:29 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -7,3 +7,6 @@ first,last = st.columns(2)
 first.text_input("First Name")
 last.text_input("Last Name")
 
+email, mob = st.columns(3,1)
+email.text_input("Enter your Email")
+mob.text_input("Mobile Number")
