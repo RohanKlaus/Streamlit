e055ece5368c40e214a2b0c6bb8e1f036bbba3b2
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 03:06:38 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -10,5 +10,7 @@ nav = st.sidebar.radio("Navigation",["Visualisation","Predictor"])
 if nav == "Visualisation":
   st.image("https://github.com/RohanKlaus/project/blob/main/customer-churn.jpg",width=800)
 
+st.text_input("Which kind of graph do you want?",["Interactive","Non-interactive"])
+
 if nav == "Predictor":
   st.write("Predict")
