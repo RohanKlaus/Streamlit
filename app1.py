90083364f8c112a2ec21610dfb684743eaa6cc08
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 00:11:03 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
+data = pd.DataFrame(
+  np.random.randn(100,3),
+  columns=['a','b','c']
+)
+
+st.write(data)
