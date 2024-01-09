d2daa88101ef5ec1bc4a12b1cef56f383ffc5829
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 00:45:41 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+#Title
 st.title('Widgets')
+
+#Button
 if st.button('Subscribe'):
   st.write('Like this Video')
+
+#Text Input
+name = st.text_input("Name")
+st.write(name)
