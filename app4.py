120fa1d4d24edb274cc1f0674e7907b7857ed09e
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-10 02:48:45 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -8,7 +8,7 @@ st.title("Telecommunication Churn")
 nav = st.sidebar.radio("Navigation",["Visualisation","Predictor"])
 
 if nav == "Visualisation":
-  st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fpulse%2Fimpact-churn-telecom-industry-how-bi-can-potentially-solve-chowdary&psig=AOvVaw0Ft646Mhn3u9P2oxQ8WnGr&ust=1704921311683000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjEzfyc0YMDFQAAAAAdAAAAABAD",width=500)
+  st.image("https://github.com/RohanKlaus/project/blob/main/customer-churn.jpg",width=500)
 
 if nav == "Predictor":
   st.write("Predict")
