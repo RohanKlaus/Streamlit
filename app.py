9b56050141b08b4ab405ed786a6a76f0be3f735f
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-09 23:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+a = [1,2,3,4,5,6,7,8]
+n = np.array(a)
+nd = n.reshape((2,4))
+dict = {
+  "name":"Sakshi",
+  "age":"20",
+  "city":"mumbai"
+}
+
+data = pd.read_csv('Churn.csv')
+print(data)
